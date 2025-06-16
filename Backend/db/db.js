2bfee30b07e8d5754d6d5d055b17590164debd3e
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-06-16 22:39:19 +0545

    connection to mongodb database done.

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 const connectDB=async(DB_URI)=>{
   try {
-    mongoose.connection.on('connected',()=>console.log('Connected to db success'));
+   await mongoose.connection.on('connected',()=>console.log('Connected to db success'));
     await mongoose.connect(DB_URI);
   } catch (error) {
     console.log('Error in connecting db',error);
