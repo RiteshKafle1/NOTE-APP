3324803565b67269c8ebe056c6d8eb788f04985b
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-06-16 22:41:28 +0545

    creating the jsonwebtoken for authentication

--- Backend/utility/token.js
+++ Backend/utility/token.js
@@ -1,10 +1,11 @@
 const webToken = require("jsonwebtoken");
-
+require('dotenv').config();
 const generateToken = async (data) => {
   try {
-    const token = webToken.sign({ userId: data }, proces.env.SECRET_KEY, {
+    const token = await webToken.sign({ userId: data }, process.env.SECRET_KEY, {
       expiresIn: "2d",
     });
+    // console.log(token);
     return token;
   } catch (error) {
     console.log("Error in generating Token", error);
